e56be0d23372f8f7e11b457fa274d539793eba10
SupriyaBankar <151713762+SupriyaBankar@users.noreply.github.com>
2024-10-17 17:46:11 +0530

    Add files via upload

---
+++ Appconfig.java
@@ -0,0 +1,10 @@
+package com.raktsanjivani.blood.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Appconfig {
+    @Autowired
+
+}
